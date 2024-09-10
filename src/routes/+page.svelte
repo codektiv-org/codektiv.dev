39c18c6bb06bfd510a93e3dc48db3ecd89ed2ba1
m93a <m93a.cz@gmail.com>
2024-09-10 13:01:21 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let count = $state(0);
+</script>
+
+<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button
+	class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900"
+	onclick={() => (count += 1)}
+>
+	Clicked {count} times
+</button>
